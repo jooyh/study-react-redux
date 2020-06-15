19703d2c6352116550018b7fd2d82d51c317db64
jooyh <1991.jyh@gmail.com>
2020-06-15 15:08:54 +0900

    React Without Redux...

---
+++ src/components/AddNumberRoot.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import AddNumber from "./AddNumber"
+
+export default class AddNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+        <AddNumber
+          onClick={function (size) {
+            console.log(size)
+            this.props.onClick(Number(size))
+          }.bind(this)}
+        ></AddNumber>
+      </div>
+    )
+  }
+}
